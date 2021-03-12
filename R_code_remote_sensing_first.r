9bb8577bbcd42df37f8b92246542e39d127b05fc
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2021-03-12 14:18:24 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -5,6 +5,6 @@
 setwd("C:/lab/") #"set working directory" ci servirà per modificare la directory, in questo caso stiamo spiegando ad R dove deve andare a prendere i file , con questa funzione dobbiamo specificare il percorso come abbiamo visto in esempio
 getwd() #"getworking directory" per scoprire la directory che stiamo usando , qui non bisogna mettere nulla nella parentesi
 #Prendiamo un file precedentemente scarcicato (p224r63_20211_masked.grd) e assegnamo un nome a scelta usando la funzione Brick
-p224r63_2011<- brick("p224r63_2011_masked.grd") #funzione Brick utilizzata per creare un raster multistrato
+p224r63_2011<-brick("p224r63_2011_masked.grd") #funzione Brick utilizzata per creare un raster multistrato
 p224r63_2011 #in questo modo richiamiamo il file e vediamo i valori interni del file
 plot(p224r63_2011) #con la funzione plot creiamo appunto un plot, che ci restituirà i valori del file ma graficamente rappresentati
