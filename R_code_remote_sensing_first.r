e849d245db62a186048ede23a787dc1c536a314f
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2021-03-17 16:15:48 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -9,3 +9,36 @@ getwd() #"getworking directory" per scoprire la directory che stiamo usando , qu
 p224r63_2011<-brick("p224r63_2011_masked.grd") #funzione Brick utilizzata per creare un raster multistrato
 p224r63_2011 #in questo modo richiamiamo il file e vediamo i valori interni del file
 plot(p224r63_2011) #con la funzione plot creiamo appunto un plot, che ci restituirà i valori del file ma graficamente rappresentati
+#avremo come risultato un plot con questi colori:
+#B1:BLUE
+#B2:GREEN
+#B3:RED
+#B4:NIR
+#B5:INFRAROSSO MEDIO
+#B6:INFRAROSSO TERMICO
+
+
+
+#Utilizziamo la funzione colorRampPalette per cambiare i colori all'interno del plot e chiamiamo questa funzione "cl"
+cl<-colorRampPalette(c('black' , 'grey' , 'light grey')) (100)  #per far capire che black, grey e lightgrey fanno parte dello stesso arcomento(colore) dobbiamo usare un VETTORE, per questo ci mettiamo una parentesi e ci mettiamo la "c" che in R indica una serie di argomento
+                                                                #il (100) indica invece il numero dei livelli dei colori e deve essere messo fuori perchè non fa parte della funzione
+
+#QUALCHE ESEMPIO CON I COLORI CAMBIATI
+clb<-colorRampPalette(c('dark blue' , 'blue' , 'light blue')) (100)
+plot(p224r63_2011, col=clb)
+clg<-colorRampPalette(c('dark green' , 'green' , 'light green')) (100)
+plot(p224r63_2011, col=clg)
+clr<-colorRampPalette(c('red' , 'green' , 'blue')) (100)
+plot(p224r63_2011, col=clr)
+cln<-colorRampPalette(c('red' , 'yellow' , 'orange')) (100)
+plot(p224r63_2011, col=cln)
+
+#richiamiamo quindi il plot con le modifiche applicate e con col definiamo il colore che preferiamo, in questo caso con il nome che abbiamo applicato prima cioè cl
+plot(p224r63_2011, col=cl)
+#Con la funzione par(mfrow=c(2,2)) indichiamo che vogliamo più plot vicini, precisamente stiamo dicendo che vogliamo dei plot in combinazione 2x2 che si distribuiscano su righe, quindi dopo il primo plot il secondo si andrà a posizionare alla sua destra, il terzo in basso al primo e così via
+par(mfrow=c(2,2)) #PER PLOT SINGOLI
+#se vogliamo invece la stessa cosa ma ordinando i plot in colonna invece useremo par(mfcol(2,2))
+par(mfcol=c(2,2)) #PER PLOT SINGOLI
+
+
+plotRGB(p224r63_2011, r=3 , g=2 , b=1, stretch="lin")
