2b48e9ad4ab301b205db00a900a5483f30312796
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2021-04-07 12:12:30 +0200

    Update 3R_code_time_series.r

--- 3R_code_time_series.r
+++ 3R_code_time_series.r
@@ -5,3 +5,23 @@
 library(raster)
 setwd("c:/lab/greenland")
 getwd()
+
+
+#diamo un nome
+
+lst_2000<-raster("lst_2000.tif")
+lst_2000
+plot(lst_2000)
+
+lst_2005<-raster("lst_2005.tif")
+lst_2010<-raster("lst_2010.tif")
+lst_2015<-raster("lst_2015.tif")
+
+
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+dev.off()
