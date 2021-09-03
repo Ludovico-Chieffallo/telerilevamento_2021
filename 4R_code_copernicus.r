51a038b2e75d442b42b66a67685f28d50643b762
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2021-09-03 13:53:45 +0200

    Update 4R_code_copernicus.r

--- 4R_code_copernicus.r
+++ 4R_code_copernicus.r
@@ -3,27 +3,32 @@
 #Sulla destra avremo la parte dedicata alla sensoristica (per scegliere correttamente possiamo andare a vedere i dettagli nella pagina principale della categoria)
 #SI seleziona il prodotto da scaricare scegliendo le date di inizio e fine di acquisizione 
 #Troveremo varie immagini con le relative date
+#Avremo dei file con estensione .nc (istalleremo un pacchetto apposito per leggere questi file)
+#Avremo un metadato .xml per l'incamerazione di tutte le informazioni riguardo le immagini
 
 
 
+#ESEMPIO
 #Soil Water Index - 10-daily SWI 12.5km Global V3
 
 
-#usiamo dati copernicus
+#Usiamo dati copernicus
+#Carichiamo il pacchetto raster poichè lavoreremo con immagini
 library(raster)
-install.packages("ncdf4") #seve proprio per leggere i file NC
-library(ncdf4)
-setwd("C:/lab/")
-getwd()
-swi<-raster("swi.nc")
-print(swi)
+install.packages("ncdf4") #Ci servirà proprio per leggere i file .nc
+library(ncdf4) #Richiamiamo il pacchetto (senza "" poichè ormai è all'interno di R)
+setwd("C:/lab/") #Selezioniamo la cartella di lavoro dove prenderemo i dati
+getwd() #Ci servirà quando non saremo sicuri della cartella nella quale stiamo lavorando
+swi<-raster("swi.nc") #Diamo un nome a piacere 
+print(swi) #vediamo le informazioni che avremo da swi
 
-
+#Creiamo un plot per vedere la rappresentazione grafica 
 plot(swi)
-cls<-colorRampPalette(c( 'red', 'blue','yellow')) (100)
+cls<-colorRampPalette(c( 'red', 'blue','yellow')) (100) #Creiamo una Palette 
 
-plot(swi, col=cls, main="SWI")
+plot(swi, col=cls, main="SWI") #Qui vediamo il pLot con la palette appena creata
+
 #ricampionamento
 swires<-aggregate(swi, fact=100)
-plot(swires,col=cls)
+plot(swires,col=cls) 
 
