90a2b410700a3bb83b62e32bff20275a22cdc91a
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2021-09-03 14:48:48 +0200

    Update 5R_code_knitr.r

--- 5R_code_knitr.r
+++ 5R_code_knitr.r
@@ -1,18 +1,21 @@
-library(knitr)
-#Definiamo la cartella di lavoro
+#Impareremo ad usare la funzione Knitr per avere un file leggibile in LaTex
 
+#Richiamiamo la funzione knitr una volta installata)
+library(knitr)
+
+#Definiamo la cartella di lavoro
 setwd("C:/lab/") 
 
 
-#usiamo un gestore di testo dove copieremo tutto il codice che vogliamo importare
-#mettiamo all'interno della nostra cartlla tutto il necessario (sia i file come immagini che il codice)
+#Usiamo un gestore di testo dove copieremo tutto il codice che vogliamo importare
+#Mettiamo all'interno della nostra cartlla tutto il necessario (sia i file come immagini che il codice)
 
-setwd("c:/lab/greenland1") #questo nel nostro caso ma bisogna selezionare la cartella corrispondente
-getwd() #per controllare la cartella di lavoro
+setwd("c:/lab/greenland1") #Questo nel nostro caso ma bisogna selezionare la cartella corrispondente
+getwd() #Per controllare la cartella di lavoro
 
-#cosa stiamo facendo in questa maniera? stiamo importando dalla cartella selezionata il file contenente il codice per creare un pdf unico
-#questo pdf avrà al suo interno tutto quello su cui abbiamo lavorato.
-#Per fare questo utilizziamo la funzione stitch dove per prima cosa metteremo il nome del file di testo che abbiamo creato
+#Cosa stiamo facendo in questa maniera? stiamo importando dalla cartella selezionata il file contenente il codice per creare un pdf unico
+#Questo pdf avrà al suo interno tutto quello su cui abbiamo lavorato.
+#Per fare questo utilizziamo la funzione stitch dove per prima cosa metteremo il nome del file di testo che abbiamo creato e salvato nella nostra cartella di riferimento.
 stitch("greenland.r", template = system.file("misc", "knitr-template.Rnw", package="knitr"))
 
-#ci restituirà un file leggibile in LaTex con tutto il codice corrispondente
+#Ci restituirà un file leggibile in LaTex con tutto il codice corrispondente
