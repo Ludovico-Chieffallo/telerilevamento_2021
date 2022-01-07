ee7a1c2a6288949c42e01bcca11dc886cbc6fa43
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2022-01-07 21:47:32 +0100

    Update 91R_code_Variability.r

--- 91R_code_Variability.r
+++ 91R_code_Variability.r
@@ -1,48 +1,61 @@
 # 10. R code variability
 
 # R_code_variability.r
+install.packages("raster")
+install.packages("RStoolbox")
+install.packages("ggplot2")
+install.packages("gridExtra")
+install.packages("viridis")
 
+#Richiamiamo le librerie
 library(raster)
 library(RStoolbox)
-# install.packages("RStoolbox")
-library(ggplot2) # for ggplot plotting
-library(gridExtra) # for plotting ggplots together
-# install.packages("viridis")
-library(viridis) # for ggplot colouring
+library(ggplot2)
+library(gridExtra) #Ci servirà per unire i plot ggplot insieme
+library(viridis) #Questo lo useremo per la colorazione del Plot
+
+#Settiamo la directory
+setwd("C:/lab/") 
 
 
-setwd("~/lab/") # Linux
-# setwd("C:/lab/") # Windows
-# setwd("/Users/name/Desktop/lab/") # Mac
-
-sent <- brick("sentinel.png")
+sent <- brick("Kourou_French_guiana.png")
+sent
 
 # NIR 1, RED 2, GREEN 3
 # r=1, g=2, b=3
 plotRGB(sent, stretch="lin") 
-# plotRGB(sent, r=1, g=2, b=3, stretch="lin") 
+#E' come se scrivessimo plotRGB(sent, r=1, g=2, b=3, stretch="lin") 
 
+#Qui possiamo giocare con bande
 plotRGB(sent, r=2, g=1, b=3, stretch="lin") 
 
-nir <- sent$sentinel.1
-red <- sent$sentinel.2
+#Dobbiamo trovare il modo di compattare tutti i nostri dati in un solo "strato"
+#Un metodo può essere utilizzare gli indici di vegetazione
 
-ndvi <- (nir-red) / (nir+red)
-plot(ndvi)
+nir <- sent$Kourou_French_Guiana.1 #Estraiamo il nir
+red <- sent$Kourou_French_Guiana.2 #Estraiamo il red
+
+ndvi <- (nir-red) / (nir+red) #Facciamo NDVI
+plot(ndvi) 
+
+#Possiamo giocare con i colori creando una nostra palette
 cl <- colorRampPalette(c('black','white','red','magenta','green'))(100) # 
 plot(ndvi,col=cl)
 
+#Usiamo la funzione focal
+#In che modo? con la fun =sd vogliamo calcolarci la deviazione standard
+#per isotropia utilizzaimo  le caselle 3x3 (isotropia=presenta le stesse proprietà in tutte le direzioni)
 ndvisd3 <- focal(ndvi, w=matrix(1/9, nrow=3, ncol=3), fun=sd)
 
 clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
 plot(ndvisd3, col=clsd)
 
-# mean ndvi with focal
+#Adesso facciamo la media sempre con la funzione focal
 ndvimean3 <- focal(ndvi, w=matrix(1/9, nrow=3, ncol=3), fun=mean)
 clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
 plot(ndvimean3, col=clsd)
 
-# changing window size
+#Cambiamo a questo punto la grandezza della moving window 
 ndvisd13 <- focal(ndvi, w=matrix(1/169, nrow=13, ncol=13), fun=sd)
 clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
 plot(ndvisd13, col=clsd)
@@ -51,13 +64,16 @@ ndvisd5 <- focal(ndvi, w=matrix(1/25, nrow=5, ncol=5), fun=sd)
 clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
 plot(ndvisd5, col=clsd)
 
-# PCA
+#C'è un'altra tecnica per compattare i dati ed è la PCA
 sentpca <- rasterPCA(sent) 
 plot(sentpca$map)  
 
-summary(sentpca$model)
+sentpca
 
-# the first PC contains 67.36804% of the original information
+#Per vedere quanta variabilità iniziale spiegano le singole componenti usiamo summary
+summary(sentpca$model)
+#La prima componente spiega 92,76146% delle informazioni originali
+
 
 pc1 <- sentpca$map$PC1
 
@@ -65,30 +81,26 @@ pc1sd5 <- focal(pc1, w=matrix(1/25, nrow=5, ncol=5), fun=sd)
 clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
 plot(pc1sd5, col=clsd)
 
-# pc1 <- sentpca$map$PC1
-# pc1sd7 <- focal(pc1, w=matrix(1/49, nrow=7, ncol=7), fun=sd)
-# plot(pc1sd7)
-
 # With the source function you can upload code from outside!
 source("source_test_lezione.r")
 source("source_ggplot.r")
 
-# https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
-# The package contains eight color scales: “viridis”, the primary choice, and five alternatives with similar properties - “magma”, “plasma”, “inferno”, “civids”, “mako”, and “rocket” -, and a rainbow color map - “turbo”.
+#https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
+#The package contains eight color scales: “viridis”, the primary choice, and five alternatives with similar properties - “magma”, “plasma”, “inferno”, “civids”, “mako”, and “rocket” -, and a rainbow color map - “turbo”.
 
 p1 <- ggplot() +
-geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
-scale_fill_viridis()  +
-ggtitle("Standard deviation of PC1 by viridis colour scale")
+  geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+  scale_fill_viridis()  +
+  ggtitle("Standard deviation of PC1 by viridis colour scale")
 
 p2 <- ggplot() +
-geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
-scale_fill_viridis(option = "magma")  +
-ggtitle("Standard deviation of PC1 by magma colour scale")
+  geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+  scale_fill_viridis(option = "magma")  +
+  ggtitle("Standard deviation of PC1 by magma colour scale")
 
 p3 <- ggplot() +
-geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
-scale_fill_viridis(option = "turbo")  +
-ggtitle("Standard deviation of PC1 by turbo colour scale")
+  geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+  scale_fill_viridis(option = "turbo")  +
+  ggtitle("Standard deviation of PC1 by turbo colour scale")
 
 grid.arrange(p1, p2, p3, nrow = 1)
